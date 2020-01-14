fd85ec3015bb8c32761c192dd3ff0e0a9204156b
Amir Javadi <ajavadi34@yahoo.com>
2020-01-13 22:35:41 -0800

    updated address protocol

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static final String apiDomain = 'http://activitylogdemo.ajdrafts.com';
+  static final String apiDomain = 'https://activitylogdemo.ajdrafts.com';
   static final String taskApiUrl = '$apiDomain/Controller/TaskController.php';
   static final String taskTypeApiUrl = '$apiDomain/Controller/TaskTypeController.php';
 }
